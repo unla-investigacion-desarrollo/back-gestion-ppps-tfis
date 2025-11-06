8d3944742a2da9051d7cd33f8ed12e66f2e9be74
Maria Eugenia Lopez <71157100+Euge16@users.noreply.github.com>
2025-11-05 22:14:21 -0300

    feat(project): agregar nuevos endpoints en project controller

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -40,8 +40,22 @@ export class ProjectController {
   @Get()
   @Roles(Role.ADMIN, Role.PROFESSOR, Role.STUDENT)
   @UseGuards(AuthGuard, RolesGuard)
-  async findAll() {
-    return await this.projectService.findAll();
+  async findAll(@Request() req: Request & { user: JwtPayload }) {
+    return await this.projectService.findAll(req.user);
+  }
+
+  @Get('my-requests')
+  @Roles(Role.STUDENT, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async getMyRequests(@Request() req: Request & { user: JwtPayload }) {
+    return this.projectService.getMyRequests(req.user);
+  }
+
+  @Get('my-projects')
+  @Roles(Role.STUDENT, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async getMyActiveProjects(@Request() req: Request & { user: JwtPayload }) {
+    return this.projectService.getMyActiveProjects(req.user);
   }
 
   @Get(':id')
@@ -70,5 +84,85 @@ export class ProjectController {
   @UseGuards(AuthGuard, RolesGuard)
   remove(@Param('id') id: string) {
     return this.projectService.remove(+id);
+  }
+
+  @Post(':id/join-professor')
+  @Roles(Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async requestJoinAsProfessor(
+    @Param('id') id: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return await this.projectService.requestJoinAsProfessor(+id, req.user);
+  }
+
+  @Post(':id/join-student')
+  @Roles(Role.STUDENT)
+  @UseGuards(AuthGuard, RolesGuard)
+  async requestJoinAsStudent(
+    @Param('id') id: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return await this.projectService.requestJoinAsStudent(+id, req.user);
+  }
+
+  @Patch(':id/approve-student/:studentRequestId')
+  @Roles(Role.ADMIN, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async approveStudentRequest(
+    @Param('id') id: string,
+    @Param('studentRequestId') studentRequestId: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return this.projectService.approveStudentRequest(
+      +id,
+      +studentRequestId,
+      req.user,
+    );
+  }
+
+  @Delete(':id/reject-student/:studentRequestId')
+  @Roles(Role.ADMIN, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async rejectStudentRequest(
+    @Param('id') id: string,
+    @Param('studentRequestId') studentRequestId: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return this.projectService.rejectStudentRequest(
+      +id,
+      +studentRequestId,
+      req.user,
+    );
+  }
+
+  @Patch(':id/approve-professor/:professorRequestId')
+  @Roles(Role.ADMIN, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async approveProfessorRequest(
+    @Param('id') id: string,
+    @Param('professorRequestId') professorRequestId: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return this.projectService.approveProfessorRequest(
+      +id,
+      +professorRequestId,
+      req.user,
+    );
+  }
+
+  @Delete(':id/reject-professor/:professorRequestId')
+  @Roles(Role.ADMIN, Role.PROFESSOR)
+  @UseGuards(AuthGuard, RolesGuard)
+  async rejectProfessorRequest(
+    @Param('id') id: string,
+    @Param('professorRequestId') professorRequestId: string,
+    @Request() req: Request & { user: JwtPayload },
+  ) {
+    return this.projectService.rejectProfessorRequest(
+      +id,
+      +professorRequestId,
+      req.user,
+    );
   }
 }
